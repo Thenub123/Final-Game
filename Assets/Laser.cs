f0eb2e04ef94164ed0a03ece0bb29bd9292ca780
Thenub123 <40504668+Thenub123@users.noreply.github.com>
2024-11-27 10:53:04 -0500

    new enemy

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -7,19 +7,59 @@ public class Laser : MonoBehaviour
 {
     public Transform player;
 
-    public LayerMask ground;
+    public LayerMask bothLayers;
+
+    public LayerMask groundLayer;
+    public LayerMask playerLayer;
 
     public LineRenderer line;
 
     public Transform dot;
 
+    public bool enemyEnabled;
+    public bool canSee;
+
+    public Vector2 prevPos;
+
+    public Transform sprite;
+
+    public Transform laserPoint;
+
     void Update() {
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.position - transform.position, 200, ground);
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.position - transform.position, 200, bothLayers);
+        RaycastHit2D rayGround = Physics2D.Raycast(transform.position, prevPos, 200, groundLayer);
 
-        line.SetPosition(1, new Vector2(ray.point.x - transform.position.x, ray.point.y - transform.position.y + 0.12f));
+        if(enemyEnabled) {
 
-        Debug.DrawRay(transform.position, player.position - transform.position);
+            canSee = Physics2D.OverlapBox(ray.point, new Vector2(0.1f, 0.1f), 0, playerLayer);
+            if(canSee) {
+                line.SetPosition(1, new Vector2(ray.point.x - transform.position.x, ray.point.y - transform.position.y));
 
-        dot.position = ray.point;
+                line.SetPosition(0, laserPoint.position - transform.position);
+
+                Debug.DrawRay(transform.position, player.position - transform.position);
+
+                dot.position = ray.point;
+
+                prevPos = new Vector2(ray.point.x - transform.position.x, ray.point.y - transform.position.y);
+
+                Vector3 relativePos = player.position - transform.position;
+                float angle = Mathf.Atan2(relativePos.y, relativePos.x) * Mathf.Rad2Deg;
+                sprite.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.layer == 13) {
+            enemyEnabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.layer == 13) {
+            enemyEnabled = false;
+            canSee = false;
+        }
     }
 }
